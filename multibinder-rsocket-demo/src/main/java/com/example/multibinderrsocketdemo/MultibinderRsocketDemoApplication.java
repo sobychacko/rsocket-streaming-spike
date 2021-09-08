0af06a3a4443a9f2cb3a9af540d1caa5655effcf
Soby Chacko <schacko@pivotal.io>
2021-09-08 12:06:33 -0400

    Simply multibinder code

--- multibinder-rsocket-demo/src/main/java/com/example/multibinderrsocketdemo/MultibinderRsocketDemoApplication.java
+++ multibinder-rsocket-demo/src/main/java/com/example/multibinderrsocketdemo/MultibinderRsocketDemoApplication.java
@@ -5,15 +5,10 @@ import java.util.function.Function;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.rsocket.messaging.RSocketStrategiesCustomizer;
-import org.springframework.cloud.function.json.JsonMapper;
-import org.springframework.cloud.function.rsocket.MessageAwareJsonDecoder;
-import org.springframework.cloud.function.rsocket.MessageAwareJsonEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.rsocket.RSocketRequester;
 
-@SpringBootApplication(excludeName = {"org.springframework.cloud.function.rsocket.RSocketAutoConfiguration",
-					"org.springframework.cloud.function.rsocket.RSocketRoutingAutoConfiguration"})
+@SpringBootApplication
 @EnableConfigurationProperties(RSocketServerProperties.class)
 public class MultibinderRsocketDemoApplication {
 
@@ -33,16 +28,5 @@ public class MultibinderRsocketDemoApplication {
 	@Bean
 	public RSocketRequester rSocketRequester(RSocketRequester.Builder rsocketRequesterBuilder, RSocketServerProperties rSocketServerProperties) {
 		return rsocketRequesterBuilder.tcp(rSocketServerProperties.getIp(), rSocketServerProperties.getPort());
-	}
-
-	@Bean
-	RSocketStrategiesCustomizer rSocketStrategiesCustomizer(JsonMapper jsonMapper) {
-		return strategies -> strategies
-				.encoders(encoders -> {
-					encoders.add(0, new MessageAwareJsonEncoder(jsonMapper, true));
-				})
-				.decoders(decoders -> {
-					decoders.add(0, new MessageAwareJsonDecoder(jsonMapper));
-				});
 	}
 }
--- uppercase-rsocket-demo/src/test/java/com/example/uppercasersocketdemo/UppercaseRsocketDemoApplicationTests.java
+++ uppercase-rsocket-demo/src/test/java/com/example/uppercasersocketdemo/UppercaseRsocketDemoApplicationTests.java
@@ -1,45 +1,14 @@
 package com.example.uppercasersocketdemo;
 
 import org.junit.jupiter.api.Test;
-import reactor.test.StepVerifier;
 
-import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.messaging.rsocket.RSocketRequester;
-import org.springframework.util.SocketUtils;
 
 @SpringBootTest
 class UppercaseRsocketDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
-
-	@Test
-	public void testWithRouteAndDefinition() {
-		int port = SocketUtils.findAvailableTcpPort();
-		try (
-				ConfigurableApplicationContext applicationContext =
-						new SpringApplicationBuilder(UppercaseRsocketDemoApplication.class)
-								.web(WebApplicationType.NONE)
-								.run("--logging.level.org.springframework.cloud.function=DEBUG",
-										"--spring.cloud.function.definition=uppercase",
-										"--spring.rsocket.server.port=" + port);
-		) {
-			RSocketRequester.Builder rsocketRequesterBuilder =
-					applicationContext.getBean(RSocketRequester.Builder.class);
-
-			rsocketRequesterBuilder.tcp("localhost", port)
-					.route("uppercase")
-					.data("hello")
-					.retrieveMono(String.class)
-					.as(StepVerifier::create)
-					.expectNext("HELLO")
-					.expectComplete()
-					.verify();
-		}
 	}
 
 }
